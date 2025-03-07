14eef6b5d3d6eada06bec1419dce68e1d82fee92
atfields <dufields@gmail.com>
2025-03-07 13:28:42 -0600

    Create Config_Make_fs_from_vcf.py

---
+++ moments/Config_Make_fs_from_vcf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#This is an example file for Configureing the moments optimization
+#This file is setup to optimize the Western Gulf splitting from the Eastern Gulf prior to the Atlantic splitting from the Eastern Gulf
+
+### Configuration file for making frequency spectrum ###
+#File named: Config_Make_fs_from_vcf.py
+
+FILE="/home/afields/Workspace/Angels/moments_Sep2023/output_WEA/dadi/West-East-Atlantic.sfs"
+POPMAP="/home/afields/Workspace/Angels/moments_Sep2023/WEA_popmap"
+POP=['West','East','Atl']
+maxiters=[3]
+folds=[3]
